b80187dbfb9df20f7ef46ac012db2c4767b8d08c
h82258652 <h82258652@hotmail.com>
2018-08-21 15:57:09 +0800

    update

---
+++ WebTest/QuickstartIdentityServer/Config.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace QuickstartIdentityServer
+{
+    public class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>()
+            {
+                new TestUser()
+                {
+                    SubjectId = "1",
+                    Username = "爱丽丝",
+                    Password = "password"
+                },
+                new TestUser()
+                {
+                    SubjectId = "2",
+                    Username = "博德",
+                    Password = "password"
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("api1", "我的 API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = {"api1"}
+                },
+                new Client()
+                {
+                    ClientId = "ro.client",
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+    }
+}
